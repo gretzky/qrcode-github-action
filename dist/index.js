e210b330fdd5671caa753ba55b84e08a92d13b42
gretzky <sfed617@gmail.com>
2022-10-14 13:08:26 -0400

    prepare

--- dist/index.js
+++ dist/index.js
@@ -9078,10 +9078,17 @@ const QRCode = __nccwpck_require__(6100);
 async function run() {
   try {
     const content = core.getInput("url", { required: true });
+    const name = core.getInput("name");
 
-    const result = await QRCode.toString(content);
+    const base64 = await QRCode.toDataURL(content);
+    const str = await QRCode.toString(content);
+    const file = await QRCode.toFile(`${process.cwd()}/${name}.png`, {
+      type: "png",
+    });
 
-    core.setOutput("qrcode", result);
+    core.setOutput("qrcodeBase64", base64);
+    core.setOutput("qrcodeStr", str);
+    core.setOutput("qrcodeImg", file);
   } catch (err) {
     core.setFailed(err.message);
   }
