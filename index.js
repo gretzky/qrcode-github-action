4af989ec8b78307b862d2bacf9a61a17b78c2b28
gretzky <sfed617@gmail.com>
2022-10-14 13:57:09 -0400

    update file

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 const core = require("@actions/core");
 const QRCode = require("qrcode");
+const stringHash = require("string-hash");
 
 async function run() {
   try {
     const content = core.getInput("url", { required: true });
-    const name = core.getInput("name");
 
     const base64 = await QRCode.toDataURL(content);
-    const str = await QRCode.toString(content);
-    const filePath = `${process.cwd()}/${name}.png`;
-
-    await QRCode.toFile(filePath, content, {
-      type: "png",
-    });
-
     core.setOutput("qrcodeBase64", base64);
+
+    const str = await QRCode.toString(content);
     core.setOutput("qrcodeStr", str);
-    core.setOutput("qrcodeImg", filePath);
+
+    const fileName = `${stringHash(content)}.png`;
+    const filePath = `${process.cwd()}/${fileName}`;
+
+    QRCode.toFile(filePath, content, { type: "png" }, (err) => {
+      if (err) throw err;
+      core.setOutput("qrcodeImg", filePath);
+    });
   } catch (err) {
     core.setFailed(err.message);
   }
--- test.js
+++ test.js
@@ -1,11 +1,27 @@
 const QRCode = require("qrcode");
+const stringHash = require("string-hash");
 
-async function main() {
-  const str =
-    "https://expo.dev/accounts/going-dev/projects/Going/builds/78a9235e-8542-4bad-8637-1d58e9b4ffc7";
-  const result = await QRCode.toFile(`./$foo.png`, str, { type: "png" });
+async function run(content, name) {
+  try {
+    const content = core.getInput("url", { required: true });
 
-  console.log(result);
+    const base64 = await QRCode.toDataURL(content);
+    const str = await QRCode.toString(content);
+    const filePath = `${process.cwd()}/${name}.png`;
+
+    await QRCode.toFile(filePath, content, {
+      type: "png",
+    });
+
+    console.log("qrcodeBase64", base64);
+    console.log("qrcodeStr", str);
+    console.log("qrcodeImg", filePath);
+  } catch (err) {
+    console.log(err.message);
+  }
 }
 
-main();
+run(
+  "https://expo.dev/accounts/going-dev/projects/Going/builds/78a9235e-8542-4bad-8637-1d58e9b4ffc7",
+  "iosQRCode"
+);
